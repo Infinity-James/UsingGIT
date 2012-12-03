61d111ac40bfe913cf914a6c7987237183a876b1
James Valaitis <james@infinityjames.com>
2012-12-03 08:34:25 +0000

    Initial Commit

---
+++ UsingGIT/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  UsingGIT
+//
+//  Created by James Valaitis on 03/12/2012.
+//  Copyright (c) 2012 &Beyond. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
